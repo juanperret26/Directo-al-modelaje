ad42c9cac48d95a2ec6a235cd7804e0ea65938a6
juan-perret <139765764+juanperret26@users.noreply.github.com>
2023-11-29 08:21:46 -0300

    prueba cambio de directorio

---
+++ go/utils/users.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/juanperret/Directo-al-modelaje/go/clients/responses"
+)
+
+const (
+	RolAdministrador = "Administrador"
+	RolUsuario       = "Usuario"
+	RolConductor     = "Conductor"
+)
+
+func SetUserInContext(c *gin.Context, user *responses.UserInfo) {
+	c.Set("UserInfo", user)
+}
+
+func GetUserInfoFromContext(c *gin.Context) *responses.UserInfo {
+	userInfo, _ := c.Get("UserInfo")
+
+	user, _ := userInfo.(*responses.UserInfo)
+
+	return user
+}
