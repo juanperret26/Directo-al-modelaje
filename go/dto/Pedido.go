ad06c39be9b4e5658999c32f705c9c02bd09d306
juan-perret <139765764+juanperret26@users.noreply.github.com>
2023-11-29 11:37:54 -0300

    Reestructuración

--- go/dto/Pedido.go
+++ go/dto/Pedido.go
@@ -4,8 +4,8 @@ package dto
 import (
 	"time"
 
-	"github.com/juanperret/Directo-al-modelaje/model"
-	"github.com/juanperret/Directo-al-modelaje/utils"
+	"github.com/juanperret/Directo-al-modelaje/go/model"
+	"github.com/juanperret/Directo-al-modelaje/go/utils"
 )
 
 type Pedido struct {
