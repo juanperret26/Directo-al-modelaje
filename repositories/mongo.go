f85339b1ce8d00d711de9970722963a36d9d4b15
Renato Williner <willinerrenato13@gmail.com>
2024-12-03 22:28:26 -0300

    Logs de mongo
    esta fallando la conexion actualmente

--- repositories/mongo.go
+++ repositories/mongo.go
@@ -2,10 +2,10 @@ package repositories
 
 import (
 	"context"
+	"log" // Importar log para depuración
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	
 )
 
 type MongoDB struct {
@@ -14,35 +14,56 @@ type MongoDB struct {
 
 func NewMongoDB() *MongoDB {
 	instancia := &MongoDB{}
-	instancia.Connect()
+	log.Println("Creando instancia de MongoDB...")
+	err := instancia.Connect()
 
+	if err != nil {
+		log.Fatalf("Error al conectar a MongoDB: %v\n", err)
+	}
+
+	log.Println("Conexión a MongoDB exitosa.")
 	return instancia
 }
 
 func (mongoDB *MongoDB) GetClient() *mongo.Client {
+	if mongoDB.Client == nil {
+		log.Println("Advertencia: El cliente de MongoDB no está inicializado.")
+	}
 	return mongoDB.Client
 }
 
 // La dejamos privada, se ejecuta cuando se crea el objeto
 func (mongoDB *MongoDB) Connect() error {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	log.Println("Intentando conectar a MongoDB...")
+	clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017")
+
+
 
 	client, err := mongo.Connect(context.Background(), clientOptions)
-
 	if err != nil {
+		log.Printf("Error al crear el cliente de MongoDB: %v\n", err)
 		return err
 	}
 
+	log.Println("Cliente de MongoDB creado, verificando la conexión con Ping...")
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
+		log.Printf("Error al hacer Ping a MongoDB: %v\n", err)
 		return err
 	}
 
+	log.Println("Ping exitoso. Conexión establecida.")
 	mongoDB.Client = client
-
 	return nil
 }
 
 func (mongoDB *MongoDB) Disconnect() error {
-	return mongoDB.Client.Disconnect(context.Background())
+	log.Println("Desconectando cliente de MongoDB...")
+	err := mongoDB.Client.Disconnect(context.Background())
+	if err != nil {
+		log.Printf("Error al desconectar el cliente de MongoDB: %v\n", err)
+		return err
+	}
+	log.Println("Cliente de MongoDB desconectado exitosamente.")
+	return nil
 }
